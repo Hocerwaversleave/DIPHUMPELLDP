4011c420064856ffe8264039327e5c4a3d53a783
Philipp Landsteiner (aka Youtube: Kanan Dethin, LDP) <kahathin39@gmail.com>
2016-09-16 11:58:29 +0200

    Second µC programm

--- RoboSync.h
+++ RoboSync.h
@@ -20,6 +20,41 @@
 //Configuration Direction Register value for both Ports
 #define IODIRABV 0x00//Outputs
 
+//SPI Register Codes:
+	//SPI Quick Commands:
+#define W_REGISTER 0x20
+#define R_RX_PAYLOAD 0x61
+#define W_TX_PAYLOAD 0xa0
+#define FLUSH_TX 0xe1
+#define NOOPERATION 0xff
+	//SPI Registers and Values:
+#define SETUP_RETR 0x04
+#define SETUP_RETRV 0x2f
+#define RF_CHPTX 0x05
+#define RF_CHPTXV 0x64//2500GHz
+#define RF_SETUP 0x06
+#define RF_SETUPV 0x06
+		
+#define STATUSPTX 0x07
+		//SPI Self created Status Codes:
+#define STATUSPTXCLEARALL 0x70
+#define STATUSPTXCLEARACKPAY 0x60
+#define STATUSPTXCLEAR_DR 0x40
+#define STATUSPTXCLEAR_DS 0x20
+#define STATUSPTXCLEAR_RT 0x10
+
+#define RX_PW_P0PTX 0x11
+#define RX_PW_P0PTXV 0x01
+
+#define FEATUREPTX 0x1d
+#define FEATUREPTXV 0x06
+
+#define DYNPDPTX 0x1c
+#define DYNPDPTXV 0x01
+
+#define CONFIGPTX 0x00
+#define CONFIGPTXV 0x0a
+
 //Robo State Codes:
 #define ELSTOPMOTO 0x01
 #define ELFORWARDS 0x02
@@ -44,7 +79,7 @@
 
 #define F_CPU 8000000UL
 
-//Hardware PIN Defines:
+//Hardware PIN Defines Robo:
 	//Port B
 #define LEDONE 0U
 #define LEDTWO 1U
@@ -66,6 +101,15 @@
 #define MOTOR_R_FWD 7U
 	//Port B Masks
 #define CLEARLEDS 0xf0
+//Hardware PIN Defines Extension PCB:
+	//Port B
+#define SPI_CSN 0U
+#define SPI_CLK 1U
+#define SPI_MOSI 2U
+#define SPI_CE 4U
+#define SPI_IRQ 5U
+#define SIGNALOTHERMCU 6U
+
 //Function prototypes:
 void
 iocon(void);
@@ -85,4 +129,18 @@ writePE(uint8_t, uint8_t);
 int8_t
 readPE(uint8_t*, uint8_t);
 
+void
+write_register_NRF(uint8_t, uint8_t);
+
+void
+write_fifo_NRF(uint8_t);
+
+void
+read_fifo_NRF(void);
+
+void
+read_status_NRF(void);
+
+void
+flush_fifotx_NRF(void);
 #endif
--- main.c
+++ main.c
@@ -1,19 +1,20 @@
 /*
- * ELRoboterMasterTakt.c
+ * ELRoboterMasterCommunication.c
  *
- * Created: 08.09.2016 09:23:56
+ * Created: 08.09.2016 18:27:57
  * Author : 1
  */ 
 
 #include <avr/io.h>
 #include <stdint.h>
-#include <avr/interrupt.h>
 #include <avr/sfr_defs.h>
 #include "RoboSync.h"
+#include <util/twi.h>
 
 void handle_mcucsr(void)
 __attribute__((section(".init3")))
 __attribute__((naked));
+
 void handle_mcucsr(void){
 	MCUSR = 0;
 	
@@ -23,135 +24,93 @@ void handle_mcucsr(void){
 	CLKPR = _BV(0);
 	
 	
-	PRR0 = _BV(PRTIM1) | _BV(PRSPI) | _BV(PRADC);
+	PRR0 = _BV(PRTIM1) | _BV(PRADC);
 	PRR1 = _BV(PRUSB) | _BV(PRTIM3) | _BV(PRUSART1);
 	ACSR |= _BV(ACD);
 }
 
-volatile uint16_t milisecs;
-
-ISR(TIMER0_OVF_vect){
-	GPIOR1++;
-	if(GPIOR1 > GPIOR2){
-		GPIOR1 = 0;
-		milisecs++;
-	}
-}
-
 int 
 main(void){
-    uint8_t contentPE;
-	enum{
-	    ELSTOP,
-	    ELFWD,
-	    ELBWD,
-	    ELTR,
-	    ELTL
-    } __attribute__((packed)) mode = ELSTOP;
-    enum{
-	    ELSTOPM,
-	    ELFWDM,
-	    ELBWDM,
-	    ELTRM,
-	    ELTLM
-    } __attribute__((packed)) modememory = ELFWDM;
-
-	iocon();
+	uint8_t contentPE;
 	
-	(void)writePE(IOCON,IOCONV);
-	(void)writePE(IODIRA,IODIRABV);
-	(void)writePE(IODIRB,IODIRABV);
+    iocon();
+	write_register_NRF(SETUP_RETR,SETUP_RETRV);
+	write_register_NRF(RF_CHPTX,RF_CHPTXV);
+	write_register_NRF(RF_SETUP,RF_SETUPV);
+	write_register_NRF(RX_PW_P0PTX,RX_PW_P0PTXV);
+	write_register_NRF(FEATUREPTX,FEATUREPTXV);
+	write_register_NRF(DYNPDPTX,DYNPDPTXV);
+	write_register_NRF(CONFIGPTX,CONFIGPTXV);
 	
 	PCIFR |= _BV(PCIF0);
-	wait(&milisecs, MILISECSTOGO);
-    
-	for(;;) {
-		wait(&milisecs, MILISECSTOGO);
-		PORTB &= CLEARLEDS;
-		
-		switch(mode) {
-			case ELSTOP:
-			drive(0,0);
-			mode = modememory;
-			
-			switch(modememory){
-				case ELSTOPM:
-				(void)writePE(OLATA,ELSTOPMOTO);
-				modememory = ELFWDM;
-				break;
-				case ELBWDM:
-				(void)writePE(OLATA,ELBACKWARD);
-				break;
-				case ELTRM:
-				(void)writePE(OLATA,ELTURNRIGH);
-				break;
-				case ELTLM:
-				(void)writePE(OLATA,ELTURNLEFT);
-				break;
-				case ELFWDM:
-				(void)writePE(OLATA,ELFORWARDS);
-			}
-			
+	
+    for(;;) {
+		loop_until_bit_is_set(PCIFR,PCIF0);
+		PCIFR |= _BV(PCIF0);
+		readPE(&contentPE, GPIOA);
+	
+		switch(contentPE) {
+			case ELSTOPMOTO:
+			write_fifo_NRF(ELSTOPMOTO);
 			break;
-			case ELFWD:
-			(void)writePE(OLATA,ELSTOPMOTO);
-			drive(3,3);
-			mode = ELSTOP;
-			modememory = ELBWDM;
+			case ELFORWARDS:
+			write_fifo_NRF(ELFORWARDS);
 			break;
-			case ELBWD:
-			(void)writePE(OLATA,ELSTOPMOTO);
-			drive(-3,-3);
-			mode = ELSTOP;
-			modememory = ELTRM;
+			case ELBACKWARD:
+			write_fifo_NRF(ELBACKWARD);
 			break;
-			case ELTR:
-			(void)writePE(OLATA,ELSTOPMOTO);
-			drive(3,-3);
-			mode = ELSTOP;
-			modememory = ELTLM;
+			case ELTURNRIGH:
+			write_fifo_NRF(ELTURNRIGH);
 			break;
-			case ELTL:
-			(void)writePE(OLATA,ELSTOPMOTO);
-			drive(-3,3);
-			mode = ELSTOP;
-			modememory = ELFWDM;
+			case ELTURNLEFT:
+			write_fifo_NRF(ELTURNLEFT);
 			break;
 			default:
-			(void)writePE(OLATA,ELERROR);
-			drive(0,0);
+			write_fifo_NRF(ELSTOPMOTO);
 		}
-		
-		SignalForOtherMCU();
+	
+		PORTB |= _BV(SPI_CE);
+	
 		loop_until_bit_is_set(PCIFR,PCIF0);
-		(void)readPE(&contentPE, GPIOA);
-		
-		switch(contentPE) {
+	
+		PORTB &= ~_BV(SPI_CE);
+		read_status_NRF();
+	
+		switch(GPIOR2) {
+			case STATUSPTXCLEARACKPAY:
+			read_fifo_NRF();
+			break;
+			case STATUSPTXCLEAR_DS:
+			GPIOR1 = RFALLOKAY;
+			break;
+			default:
+			GPIOR1 = ELERROR;
+			flush_fifotx_NRF();
+		}
+	
+		switch(GPIOR1) {
 			case RFALLOKAY:
-			PORTB |= _BV(PORTB7);
-			while(milisecs < (milisecs + 70));
-			PORTB &= ~_BV(PORTB7);
+			writePE(OLATA,RFALLOKAY);
 			break;
 			case RFLEDFRIG:
-			PORTB |= _BV(LEDONE);
+			writePE(OLATA,RFLEDFRIG);
 			break;
 			case RFLEDBRIG:
-			PORTB |= _BV(LEDTWO);
+			writePE(OLATA,RFLEDBRIG);
 			break;
 			case RFLEDFLEF:
-			PORTB |= _BV(LEDTHREE);
+			writePE(OLATA,RFLEDFLEF);
 			break;
 			case RFLEDBLEF:
-			PORTB |= _BV(LEDFOUR);
+			writePE(OLATA,RFLEDBLEF);
 			break;
 			default:
-			drive(0,0);
-			mode = ELSTOP;
-			modememory = ELSTOPM;
-			wait(&milisecs, ERRORWAITTIME);
+			writePE(OLATA,ELERROR);
 		}
-		
+	
+		SignalForOtherMCU();
+		write_register_NRF(STATUSPTX,STATUSPTXCLEARALL);
 		PCIFR |= _BV(PCIF0);
-    }
+	}
 }
 
